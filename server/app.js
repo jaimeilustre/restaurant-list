261095daab62c71ab1e96de597799a92f7d5d641
Jaime Ilustre <jaimeilustre@yahoo.com>
2026-04-11 12:17:11 +0200

    moved server logic into app.js file so that server.js just runs the server

---
+++ server/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+const FRONTEND_URL = "http://localhost:3000"; // Change later when deployed
+
+// For testing later once frontend is setup
+app.use(
+	cors({
+		origin: [FRONTEND_URL]
+	})
+);
+
+app.get('/', (req, res) => {
+	res.send("Testing 1, 2, 3");
+});
+
+export default app;
--- server/server.js
+++ server/server.js
@@ -1,21 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
+import app from './app.js'
 
 const PORT = 8080;
-
-const FRONTEND_URL = "http://localhost:3000"; // Change later when deployed
-
-// For testing later once frontend is setup
-app.use(
-	cors({
-		origin: [FRONTEND_URL]
-	})
-);
-
-app.get('/', (req, res) => {
-	res.send("Testing 1, 2, 3");
-});
 
 app.listen(PORT, () => {
 	console.log(`Server running on localhost:${PORT}`);
