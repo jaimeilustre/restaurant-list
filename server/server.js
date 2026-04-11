09b86a500f631d4d923633156905549dc538f91a
Jaime Ilustre <jaimeilustre@yahoo.com>
2026-04-11 11:41:18 +0200

    added CORS for testing later once frontend is setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
-const express = require("express");;
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const PORT = 8080;
+
+const FRONTEND_URL = "http://localhost:3000"; // Change later when deployed
+
+// For testing later once frontend is setup
+app.use(
+	cors({
+		origin: [FRONTEND_URL]
+	})
+);
 
 app.get('/', (req, res) => {
 	res.send("Testing 1, 2, 3");
