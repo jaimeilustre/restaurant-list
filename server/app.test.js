31f6f16fd016560120c581796b1a722e450701a5
Jaime Ilustre <jaimeilustre@yahoo.com>
2026-04-13 14:07:46 +0200

    wrote test to test mock call with axios for 1 restaurant

--- server/app.test.js
+++ server/app.test.js
@@ -1,6 +1,14 @@
 import app from './app.js';
 import request from 'supertest';
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest';
+import axios from 'axios';
+
+vi.mock('axios' , () => ({
+	// method: vi.fn()
+	default: {
+		get: vi.fn()
+	}
+}));
 
 describe ('GET /restaurants/:postcode', () => {
 	it('Return 200 when successful', async () => {
@@ -12,4 +20,17 @@ describe ('GET /restaurants/:postcode', () => {
 		const res = await request(app).get('/restaurants/EC4M7RF');
 		expect(res.body).toHaveProperty('restaurants');
 	})
-})
+
+	it ('Return restaurant data for 1 restaurant', async () => {
+		axios.get.mockResolvedValue({
+			data: {
+				restaurants: [{ name: 'Test' }]
+			}
+		});
+
+		const res = await request(app).get('/restaurants/EC4M7RF');
+		expect(res.body.restaurants.length).toBe(1);
+	})
+});
+
+
