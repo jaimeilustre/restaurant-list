b4609f4da1b85b054e2cd4fa1923d9db1feb5e44
Jaime Ilustre <jaimeilustre@yahoo.com>
2026-04-13 15:23:35 +0200

    changed path for testing specific postcodes when writing the restaurant list page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <Routes>
 		<Route path="/" element={<HomePage />} />
-		<Route path="/restaurants" element={<RestaurantListPage />} />
+		<Route path="restaurants/:postcode" element={<RestaurantListPage />} />
 	  </Routes>
     </>
   )
