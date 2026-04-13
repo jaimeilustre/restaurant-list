e95ae283416984322975230fc1a2b8762f5422e1
Jaime Ilustre <jaimeilustre@yahoo.com>
2026-04-13 20:10:24 +0200

    added test to test async behaviour with loading text

--- client/src/pages/RestaurantListPage.test.jsx
+++ client/src/pages/RestaurantListPage.test.jsx
@@ -7,7 +7,11 @@ import axios from 'axios';
 describe('Restaurant List Page', () => {
 	it ('Render header', () => {
 		render(<RestaurantListPage />);
-
 		expect(screen.getByText('List of restaurants')).toBeInTheDocument();
 	});
+
+	it('Shows loading word to test async behaviour', () => {
+		render(<RestaurantListPage />);
+		expect(screen.getByText('Loading...')).toBeInTheDocument();
+	})
 });
