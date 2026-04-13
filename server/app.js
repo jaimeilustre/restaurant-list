1c6a76fe00f3263219dee0099cb6bd69ab8296bd
Jaime Ilustre <jaimeilustre@yahoo.com>
2026-04-13 14:56:12 +0200

    wrapped GET route in a try/catch block to pass API call fail test

--- server/app.js
+++ server/app.js
@@ -21,11 +21,21 @@ app.get('/restaurants/:postcode', async (req, res) => {
 	// res.status(200).json({
 	// 	restaurants: [{name: "Test"}]
 	// });
+	// const apiUrl = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/";
+	// const { postcode } = req.params;
+	
+	// const response = await axios.get(`${apiUrl}${postcode}`);
+	// res.json(response.data);
+
 	const apiUrl = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/";
 	const { postcode } = req.params;
 	
-	const response = await axios.get(`${apiUrl}${postcode}`);
-	res.json(response.data);
+	try {
+		const response = await axios.get(`${apiUrl}${postcode}`);
+		res.json(response.data);
+	} catch (err) {
+		res.status(500).json({ message: "Error getting postcode" });
+	}
 })
 
 
