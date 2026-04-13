c4dc80b3c9ff324299a7d1e40a995b9c5691d896
Jaime Ilustre <jaimeilustre@yahoo.com>
2026-04-13 22:34:04 +0200

    added more restaurant details to display and protected undefined values with ?

--- client/src/pages/RestaurantListPage.jsx
+++ client/src/pages/RestaurantListPage.jsx
@@ -24,11 +24,17 @@ function RestaurantListPage() {
 	return (
 		<>
 			<h1>List of restaurants</h1>
-			{restaurants == null
+			{!restaurants
 			? <p>Loading...</p>
 			// : <p>Loading complete!</p>
-			: restaurants.map(r => (
-				<h1 key={r.id}>{r.name}</h1>
+			: restaurants.map(restaurant => (
+				// <h1 key={r.id}>{r.name}</h1>
+				<div key={restaurant.id}>
+					<h1>{restaurant.name}</h1>
+					<h1>{restaurant.cuisines?.map(c => c.name).join(', ')}</h1>
+					<h1>{restaurant.rating?.starRating} ({restaurant.rating?.count})</h1>
+					<h1>{restaurant.address?.firstLine}</h1>
+				</div>
 			))
 			}
 		</>
