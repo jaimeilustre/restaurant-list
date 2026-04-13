849172acd99a18e43bfbe582b00c43621af25d9d
Jaime Ilustre <jaimeilustre@yahoo.com>
2026-04-13 20:52:14 +0200

    wrote getRestaurants function to use mock axios call to a test url to pass previously written test with useState and useEffect

--- client/src/pages/RestaurantListPage.jsx
+++ client/src/pages/RestaurantListPage.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function RestaurantListPage() {
 
 	const [restaurants, setRestaurants] = useState(null);
+
+	const getRestaurants = async () => {
+		try {
+			const res = await axios.get('test url');
+			setRestaurants(res.data.restaurants);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	useEffect(() => {
+		getRestaurants();
+	}, [])
 
 	return (
 		<>
 			<h1>List of restaurants</h1>
 			{restaurants == null
 			? <p>Loading...</p>
-			: <p>Loading complete!</p>
+			// : <p>Loading complete!</p>
+			: restaurants.map(r => (
+				<h1 key={r.id}>{r.name}</h1>
+			))
 			}
 		</>
 		
