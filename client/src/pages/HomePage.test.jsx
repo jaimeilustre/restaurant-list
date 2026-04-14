cf502733da57ba866e89da5cd6eacf9dd765e7b3
Jaime Ilustre <jaimeilustre@yahoo.com>
2026-04-14 08:44:41 +0200

    wrote test to test navigation to restaurant list page when search button is clicked after inputting postcode

--- client/src/pages/HomePage.test.jsx
+++ client/src/pages/HomePage.test.jsx
@@ -1,15 +1,27 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import { describe, it } from 'vitest';
+import { describe, it, vi } from 'vitest';
 import '@testing-library/jest-dom';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import * as router from 'react-router-dom';
 import HomePage from './HomePage';
+
+const mockNavigate = vi.fn();
+
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+
+  return {
+    ...actual,
+    useNavigate: () => mockNavigate,
+  };
+});
 
 describe('Home page with search function', () => {
 	it('Render header', () => {
 		render(<HomePage />);
 		expect(screen.getByText('Home page')).toBeInTheDocument();
-	})
-	
+	});
+
 	it('Test input typing', () => {
 		render(
 			<MemoryRouter>
@@ -20,5 +32,23 @@ describe('Home page with search function', () => {
 		const input = screen.getByPlaceholderText('Type postcode here');
 		fireEvent.change(input, { target: { value: 'EC4M7RF'}});
 		expect(input.value).toBe('EC4M7RF');
+	});
+
+	it('Navigate to restaurant list page when postcode is inputed', () => {
+		render(
+			<MemoryRouter>
+				<HomePage />
+			</MemoryRouter>
+		);
+
+		const input = screen.getByPlaceholderText('Type postcode here');
+		const button = screen.getByText('Search');
+
+		fireEvent.change(input, { target: { value: 'EC4M7RF'}});
+		fireEvent.click(button);
+
+		expect(mockNavigate).toHaveBeenCalledWith('restaurants/EC4M7RF');
 	})
+
+
 })
