c18a69170db4cf0cd7b279eea7aa73478c51fcce
Jaime Ilustre <jaimeilustre@yahoo.com>
2026-04-14 08:28:14 +0200

    created an input field where to type postcode

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,25 @@
+import { useState } from 'react';
+
 function HomePage() {
+
+	const [searchPostcode, setSearchPostcode] = useState('');
+
+	const searchHandler = (e) => {
+		setSearchPostcode(e.target.value);
+	}
+
 	return (
+		<>
 		<h1>Home page</h1>
+
+		<h1>Search by postcode</h1>
+			<input
+				type="text"
+				value={searchPostcode}
+				onChange={searchHandler}
+				placeholder="Type postcode here"
+			/>
+		</>
 	)
 }
 
