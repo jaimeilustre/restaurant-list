72e5e9e0b6e94aa605a77f679612a198a36a2bd6
Jaime Ilustre <jaimeilustre@yahoo.com>
2026-04-14 08:17:46 +0200

    added test to test input typing

--- client/src/pages/HomePage.test.jsx
+++ client/src/pages/HomePage.test.jsx
@@ -1,11 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { describe, it } from 'vitest';
 import '@testing-library/jest-dom';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage';
 
 describe('Home page with search function', () => {
 	it('Render header', () => {
 		render(<HomePage />);
 		expect(screen.getByText('Home page')).toBeInTheDocument();
+	})
+	
+	it('Test input typing', () => {
+		render(
+			<MemoryRouter>
+				<HomePage />
+			</MemoryRouter>
+		);
+		
+		const input = screen.getByPlaceholderText('Type postcode here');
+		fireEvent.change(input, { target: { value: 'EC4M7RF'}});
+		expect(input.value).toBe('EC4M7RF');
 	})
 })
