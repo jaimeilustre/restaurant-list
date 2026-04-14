d8dfaad3b499066cb1681b46e7b70caf62d9a8dd
Jaime Ilustre <jaimeilustre@yahoo.com>
2026-04-14 08:50:26 +0200

    added searchButtonHandler to navigate to restaurant list page when search button is clicked

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,24 +1,32 @@
 import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
 
 	const [searchPostcode, setSearchPostcode] = useState('');
+	const navigate = useNavigate();
 
 	const searchHandler = (e) => {
 		setSearchPostcode(e.target.value);
 	}
 
+	const searchButtonHandler = (e) => {
+		navigate(`restaurants/${searchPostcode}`);
+	}
+
 	return (
 		<>
-		<h1>Home page</h1>
+			<h1>Home page</h1>
 
-		<h1>Search by postcode</h1>
+			<h1>Search by postcode</h1>
 			<input
 				type="text"
 				value={searchPostcode}
 				onChange={searchHandler}
 				placeholder="Type postcode here"
 			/>
+
+			<button onClick={searchButtonHandler}>Search</button>
 		</>
 	)
 }
